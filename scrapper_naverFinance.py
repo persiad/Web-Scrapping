4e255d76afa76ef415639595a8e0bee532744a13
persiad <109460168+persiad@users.noreply.github.com>
2023-12-30 14:42:28 -0500

    Update scapper.py
    1. make sure to install package lxml
    2. Edited df.append vs pd.concat in get_data

--- scrapper_naverFinance.py
+++ scrapper_naverFinance.py
@@ -1,48 +1,56 @@
-#Hynix PlotChart 
+# Hynix PlotChart
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
-import mplfinance as mpf
+# import mplfinance as mpf
 
 def main():
     url = "https://finance.naver.com/item/sise_day.nhn?code=000660&page=1"
-    last=max_page(url)
-    data=get_data(url,last)
+    last = max_page(url)
+    data = get_data(url, last)
     display(data)
-  
-def max_page(url):
+
+
+def max_page(url: str) -> int:
     headers = {
         'user-agent': "Mozilla/5.0"
     }
     result = requests.get(url, headers=headers)
+    # make sure to install lxml package; otherwise the next line will not run
     soup = BeautifulSoup(result.text, "lxml")
-    pgrr = soup.find("td",{"class":"pgRR"}).a['href'].split("=")
-    max_page = pgrr[-1]
-    return(max_page)
+    pgrr = soup.find("td", {"class": "pgRR"}).a['href'].split("=")
+    max_page: int = pgrr[-1]
+    return (max_page)
 
-def get_data(url,last):
+
+def get_data(url: str, last: int):
     url = url[:-7]
     df = pd.DataFrame()
-    for page in range(1,int(last)+1):
+    for page in range(1, int(last) + 1):
         page_url = f"{url}&page={page}"
         headers = {
-        'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+            'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
         }
         result = requests.get(page_url, headers=headers)
         data = pd.read_html(result.text, header=0)[0]
-        df = df.append(data, ignore_index=True)
+        # you can use either of the following lines: append or concatenate
+        # df = df.append(data, ignore_index=True)
+        df = pd.concat([df, data], ignore_index=True)
     df = df.dropna()
     df = df.sort_values(by="날짜")
     return (df)
     # df.append(pd.read_html(data))
     # return (df)
 
+
 def display(data):
     df = data
     plt.title('Hynix (close)')
-    plt.xticks(rotation=45) #rotate dates cuz other wise they'll overlap one another
-    plt.plot(df['날짜'], df['종가'], 'co-') #co = cyan color
+    plt.xticks(rotation=45)  # rotate dates cuz other wise they'll overlap one another
+    plt.plot(df['날짜'], df['종가'], 'co-')  # co = cyan color
     plt.grid(color='gray', linestyle='--')
     plt.show()
+
+
 main()
